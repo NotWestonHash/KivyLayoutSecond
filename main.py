cc0d0ccb1bd5f18bb3c2cfb6243e16cee2570d1c
NotWestonHash <elijahsmykla@gmail.com>
2023-09-22 12:38:56 -0500

    You Must Fill Out Assignment Layout And Get Rid Of The Junk.

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
+
+class AssignmentLayout(BoxLayout):
+    pass
 
 
 class MainWidget(Widget):
     pass
 
 
-class WidgetLayoutApp(App):
+class BoxLayoutExample(BoxLayout):
     pass
 
 
-WidgetLayoutApp().run()
+class GridLayoutExample(GridLayout):
+    pass
+
+
+class StackLayoutExample(StackLayout):
+    pass
+
+
+class KivyLayoutApp(App):
+    pass
+
+
+KivyLayoutApp().run()
